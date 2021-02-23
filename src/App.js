676ccb1813922ed6171b3b058f245ece2f3aff11
Linh Pham <thuytpham9@gmail.com>
2021-02-23 08:49:54 -0800

    Fetch data and create BookCard & Search components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import BookCard from './components/BookCard';
+import Search from './components/Search';
 
-function App() {
+export default function App() {
+  const [books, setBooks] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    searchIBN().then()
+  }, [input])
+
+  async function searchIBN() {
+    const response = await fetch (
+      `https://openlibrary.org/api/books?bibkeys=ISBN:${input}&jscmd=data&format=json`
+    );
+    const data = await response.json()
+    setBooks(data)
+  }
+
+  const searchUserInput = (e) => {
+    setInput(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Router>
+        <Search input={input} onClick={searchIBN} onChange={searchUserInput} />
+        <Switch>
+          <Route path={`/isbn/${input}`}>
+          <BookCard bookData={books}/>
+          </Route>
+        </Switch>
+      </Router>
 
-export default App;
+    </div>
+  )
+}
